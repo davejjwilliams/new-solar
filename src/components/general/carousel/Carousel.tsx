8dade230073d986c48c8ecf84bd7a6c1ea9aa110
davejjwilliams <davejjwilliams@gmail.com>
2025-06-07 22:41:29 +0100

    Cleaned up Basic Carousel

--- src/components/general/carousel/Carousel.tsx
+++ src/components/general/carousel/Carousel.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useEffect } from 'react'
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import WindowCard from '../WindowCard'
 import '../../../styles/Carousel.css'
 
 const HomeCarousel = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 2000 })])
     useEffect(() => {
         if (emblaApi) {
             console.log(emblaApi.slideNodes()) // Access API
@@ -16,7 +17,15 @@ const HomeCarousel = () => {
     const scrollNext = useCallback(() => { if (emblaApi) emblaApi.scrollNext() }, [emblaApi])
 
     return (
-        <WindowCard title="Carousel Example">
+        <WindowCard title="News">
+            <div className="embla__buttons">
+                <button className="embla__button embla__prev" onClick={scrollPrev}>
+                    <IoChevronBackOutline />
+                </button>
+                <button className="embla__button embla__next" onClick={scrollNext}>
+                    <IoChevronForwardOutline />
+                </button>
+            </div>
             <div className="embla" ref={emblaRef}>
                 <div className="embla__container">
                     <div className="embla__slide">
@@ -29,12 +38,6 @@ const HomeCarousel = () => {
                         <h1>This is slide 3.</h1>
                     </div>
                 </div>
-                <button className="embla__prev" onClick={scrollPrev}>
-                    Prev
-                </button>
-                <button className="embla__next" onClick={scrollNext}>
-                    Next
-                </button>
             </div>
         </WindowCard>
     )
