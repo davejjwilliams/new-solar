37e03dc23c6b1f21a5260856b1f83d9a73413344
davejjwilliams <davejjwilliams@gmail.com>
2025-06-15 15:38:04 +0100

    Join us page

--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -1,14 +1,60 @@
 import type { FunctionComponent } from "react";
-import TextLink from "../components/general/TextLink";
+import { FaLightbulb, FaLocationArrow, FaEnvelope, FaPaperclip } from "react-icons/fa";
+
 
 type JoinUsProps = object;
 
 const JoinUs: FunctionComponent<JoinUsProps> = () => {
     return (
         <>
-            <div className="page-title">Work With Us</div>
-            <p>We are looking for talented people who are interested in joining SOLAR as visiting scholars (at all levels), PhD students, Research Assistants and Research Fellows.</p>
-            <p> If you are interested, send an e-mail to <TextLink url="mailto:f.sarro@ucl.ac.uk" text="Prof. Federica Sarro"/> and include your CV, a statement of your research interests and experience, ideal starting time (and transcripts if you are a student) in your message.</p>
+            <div className="smaller-content">
+                <div className="page-title">Work With Us</div>
+                <div className="contact-logo">
+                    <a href="mailto:f.sarro@ucl.ac.uk">
+                        <img src="/assets/logo/join-us-logo.png" alt="Join Us Logo" className="logo" />
+                    </a>
+                </div>
+
+
+                <div className="panel glass">
+                    <div className="centred">
+                        We are looking for passionate and talented people to join our team at SOLAR!
+                    </div>
+                </div>
+
+                <div className="columns-container">
+                    <div className="column">
+                        <div className="panel glass">
+                            <div className="panel-title"><FaLightbulb /> Open Positions:</div>
+                            <ul>
+                                <li>Visiting Scholars (all levels)</li>
+                                <li>PhD Students</li>
+                                <li>Research Assistants</li>
+                                <li>Research Fellows</li>
+                            </ul>
+                        </div>
+                        <a href="mailto:f.sarro@ucl.ac.uk" className="contact-button">Contact Us</a>
+                    </div>
+                    <div className="column">
+                        <div className="panel glass">
+                            <div className="panel-title"><FaLocationArrow /> Location:</div>
+                            <p>University College London (UCL)</p>
+
+                            <div className="panel-title"><FaEnvelope /> Contact:</div>
+                            <p>Prof. Federica Sarro<br />
+                                f.sarro@ucl.ac.uk</p>
+
+                            <div className="panel-title"><FaPaperclip /> What to Include:</div>
+                            <ul>
+                                <li>CV</li>
+                                <li>Statement of research interests</li>
+                                <li>Experience & ideal starting date</li>
+                                <li>Transcript (if you're a student)</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
