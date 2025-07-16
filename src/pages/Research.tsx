61150360229aae9180ea446b0a04a4352d2fb7b4
davejjwilliams <davejjwilliams@gmail.com>
2025-07-16 19:18:24 +0100

    Fix Build

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -57,61 +57,61 @@ const researchTopics = [
   }
 ];
 
-const researchProjects = [
-  {
-    title: "Bias Mitigation Survey",
-    paperName: "Bias Mitigation for Machine Learning Classifiers: A Comprehensive Survey, Journal on Responsible Computing",
-    venue: "Journal on Responsible Computing",
-    description: "This paper provides a comprehensive survey of bias mitigation methods for achieving fairness in Machine Learning (ML) models. We collect a total of 341 publications concerning bias mitigation for ML classifiers. These methods can be distinguished based on their intervention procedure (i.e., pre-processing, in-processing, post-processing) and the technique they apply. We investigate how existing bias mitigation methods are evaluated in the literature. In particular, we consider datasets, metrics and benchmarking. Based on the gathered insights (e.g., What is the most popular fairness metric? How many datasets are used for evaluating bias mitigation methods?), we hope to support practitioners in making informed choices when developing and evaluating new bias mitigation methods.",
-    links: [
-      { text: "Preprint", url: "https://solar.cs.ucl.ac.uk/pdf/hort2023fairness.pdf" },
-      { text: "Online Appendix", url: "https://docs.google.com/spreadsheets/d/1kOmbKLMiFgHRSXvgM-O8OW4YKeDIGN0cPPeCGQOMnnA/edit?gid=0#gid=0" },
-    ],
-    authors: [
-      "Max Hort",
-      "Zhenpeng Chen",
-      "Jie Zhang",
-      "Federica Sarro",
-      "Mark Harman",
-    ],
-    acknolwedgement: "EPIC",
-  },
-  {
-    title: "Fairea",
-    paperName: "A Model Behaviour Mutation Approach to Benchmarking Bias Mitigation Methods",
-    venue: "ESEC/FSE 2021",
-    description: "The increasingly wide uptake of Machine Learning (ML) has raised the significance of the problem of tackling bias (i.e., unfairness), making it a primary software engineering concern. In this paper, we introduce Fairea, a model behaviour mutation approach to benchmarking ML bias mitigation methods. We also report on a largescale empirical study to test the effectiveness of 12 widely-studied bias mitigation methods. Our results reveal that, surprisingly, bias mitigation methods have a poor effectiveness in 49% of the cases. In particular, 15% of the mitigation cases have worse fairness-accuracy trade-offs than the baseline established by Fairea; 34% of the cases have a decrease in accuracy and an increase in bias. Fairea has been made publicly available for software engineers and researchers to evaluate their bias mitigation methods.",
-    links: [
-      { text: "Preprint", url: "https://solar.cs.ucl.ac.uk/pdf/hort2021fairea.pdf" },
-      { text: "Replication Package", url: "https://github.com/maxhort/Fairea" },
-    ],
-    authors: [
-      "Max Hort",
-      "Jie Zhang",
-      "Federica Sarro",
-      "Mark Harman",
-    ],
-    acknolwedgement: "EPIC",
-  },
-  {
-    title: "Learning From Mistakes",
-    paperName: "Machine Learning Enhanced Human Expert Effort Estimates",
-    venue: "IEEE TSE",
-    description: "Learning From Mistakes (LFM) is a novel approach to predictive modeling for software engineering. The core idea underlying our proposal is to automatically learn from past estimation errors made by human experts, in order to predict the characteristics of their future misestimates, therefore resulting in improved future estimates. We show the feasibility of LFM by investigating whether it is possible to predict the type, severity and magnitude of errors made by human experts when estimating the development effort of software projects, and whether it is possible to use these predictions to enhance future estimations. To this end we conduct a thorough empirical study investigating 402 maintenance and new development industrial software projects. The results of our study reveal that the type, severity and magnitude of errors are all, indeed, predictable. Moreover, we find that by exploiting these predictions, we can obtain significantly better estimates than those provided by random guessing, human experts and traditional machine learners in 31 out of the 36 cases considered (86%), with large and very large effect sizes in the majority of these cases (81%). This empirical evidence opens the door to the development of techniques that use the power of machine learning, coupled with the observation that human errors are predictable, to support engineers in estimation tasks rather than replacing them with machine-provided estimates.",
-    links: [
-      { text: "Preprint", url: "http://www0.cs.ucl.ac.uk/staff/F.Sarro/resource/papers/TSE2020_LFM.pdf" },
-      { text: "Publisher Link", url: "https://ieeexplore.ieee.org/abstract/document/9272884" },
-      { text: "Appendix", url: "https://solar.cs.ucl.ac.uk/os/lfm/LFM-Appendix.pdf" },
-    ],
-    authors: [
-      "Federica Sarro",
-      "Rebecca Moussa",
-      "Alessio Petrozziello",
-      "Mark Harman", ,
-    ],
-    acknolwedgement: "EPIC",
-  },
-]
+// const researchProjects = [
+//   {
+//     title: "Bias Mitigation Survey",
+//     paperName: "Bias Mitigation for Machine Learning Classifiers: A Comprehensive Survey, Journal on Responsible Computing",
+//     venue: "Journal on Responsible Computing",
+//     description: "This paper provides a comprehensive survey of bias mitigation methods for achieving fairness in Machine Learning (ML) models. We collect a total of 341 publications concerning bias mitigation for ML classifiers. These methods can be distinguished based on their intervention procedure (i.e., pre-processing, in-processing, post-processing) and the technique they apply. We investigate how existing bias mitigation methods are evaluated in the literature. In particular, we consider datasets, metrics and benchmarking. Based on the gathered insights (e.g., What is the most popular fairness metric? How many datasets are used for evaluating bias mitigation methods?), we hope to support practitioners in making informed choices when developing and evaluating new bias mitigation methods.",
+//     links: [
+//       { text: "Preprint", url: "https://solar.cs.ucl.ac.uk/pdf/hort2023fairness.pdf" },
+//       { text: "Online Appendix", url: "https://docs.google.com/spreadsheets/d/1kOmbKLMiFgHRSXvgM-O8OW4YKeDIGN0cPPeCGQOMnnA/edit?gid=0#gid=0" },
+//     ],
+//     authors: [
+//       "Max Hort",
+//       "Zhenpeng Chen",
+//       "Jie Zhang",
+//       "Federica Sarro",
+//       "Mark Harman",
+//     ],
+//     acknolwedgement: "EPIC",
+//   },
+//   {
+//     title: "Fairea",
+//     paperName: "A Model Behaviour Mutation Approach to Benchmarking Bias Mitigation Methods",
+//     venue: "ESEC/FSE 2021",
+//     description: "The increasingly wide uptake of Machine Learning (ML) has raised the significance of the problem of tackling bias (i.e., unfairness), making it a primary software engineering concern. In this paper, we introduce Fairea, a model behaviour mutation approach to benchmarking ML bias mitigation methods. We also report on a largescale empirical study to test the effectiveness of 12 widely-studied bias mitigation methods. Our results reveal that, surprisingly, bias mitigation methods have a poor effectiveness in 49% of the cases. In particular, 15% of the mitigation cases have worse fairness-accuracy trade-offs than the baseline established by Fairea; 34% of the cases have a decrease in accuracy and an increase in bias. Fairea has been made publicly available for software engineers and researchers to evaluate their bias mitigation methods.",
+//     links: [
+//       { text: "Preprint", url: "https://solar.cs.ucl.ac.uk/pdf/hort2021fairea.pdf" },
+//       { text: "Replication Package", url: "https://github.com/maxhort/Fairea" },
+//     ],
+//     authors: [
+//       "Max Hort",
+//       "Jie Zhang",
+//       "Federica Sarro",
+//       "Mark Harman",
+//     ],
+//     acknolwedgement: "EPIC",
+//   },
+//   {
+//     title: "Learning From Mistakes",
+//     paperName: "Machine Learning Enhanced Human Expert Effort Estimates",
+//     venue: "IEEE TSE",
+//     description: "Learning From Mistakes (LFM) is a novel approach to predictive modeling for software engineering. The core idea underlying our proposal is to automatically learn from past estimation errors made by human experts, in order to predict the characteristics of their future misestimates, therefore resulting in improved future estimates. We show the feasibility of LFM by investigating whether it is possible to predict the type, severity and magnitude of errors made by human experts when estimating the development effort of software projects, and whether it is possible to use these predictions to enhance future estimations. To this end we conduct a thorough empirical study investigating 402 maintenance and new development industrial software projects. The results of our study reveal that the type, severity and magnitude of errors are all, indeed, predictable. Moreover, we find that by exploiting these predictions, we can obtain significantly better estimates than those provided by random guessing, human experts and traditional machine learners in 31 out of the 36 cases considered (86%), with large and very large effect sizes in the majority of these cases (81%). This empirical evidence opens the door to the development of techniques that use the power of machine learning, coupled with the observation that human errors are predictable, to support engineers in estimation tasks rather than replacing them with machine-provided estimates.",
+//     links: [
+//       { text: "Preprint", url: "http://www0.cs.ucl.ac.uk/staff/F.Sarro/resource/papers/TSE2020_LFM.pdf" },
+//       { text: "Publisher Link", url: "https://ieeexplore.ieee.org/abstract/document/9272884" },
+//       { text: "Appendix", url: "https://solar.cs.ucl.ac.uk/os/lfm/LFM-Appendix.pdf" },
+//     ],
+//     authors: [
+//       "Federica Sarro",
+//       "Rebecca Moussa",
+//       "Alessio Petrozziello",
+//       "Mark Harman", ,
+//     ],
+//     acknolwedgement: "EPIC",
+//   },
+// ]
 
 const Research: FunctionComponent<ResearchProps> = () => {
   return (
